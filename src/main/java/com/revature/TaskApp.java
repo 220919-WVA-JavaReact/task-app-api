902b7970c7e91f5cc7a221e18ad19833c5ff9662
Kevin Tran Huu <kevin.tranhuu@revature.com>
2022-10-28 15:44:23 -0400

    establishing starting point of spring boot app

---
+++ src/main/java/com/revature/TaskApp.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class TaskApp {
+    public static void main(String[] args){
+        SpringApplication.run(TaskApp.class, args);
+    }
+}
